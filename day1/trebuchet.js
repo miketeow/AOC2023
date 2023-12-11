d9ab93a3bf865ec98d60f12a0ad75ec38e2044e9
miketeow <27071053+miketeow@users.noreply.github.com>
2023-12-11 13:12:44 +0800

    AOC Day 1 full_solution(my)

--- day1/trebuchet.js
+++ day1/trebuchet.js
@@ -1,4 +1,3 @@
-// const lines = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet", "8451"];
 const fs = require("fs").promises;
 const filePath = "input.txt";
 async function readLines(filePath) {
@@ -11,21 +10,68 @@ async function readLines(filePath) {
     console.error("Error reading file " + filePath);
   }
 }
-function getNumberFromString(str) {
-  const regex = /\d/g;
-  const matches = str.match(regex);
 
-  return matches;
+function getFirstDigit(str) {
+  const regex = /one|two|three|four|five|six|seven|eight|nine|[1-9]/;
+  const matches = str.match(regex);
+  const firstDigit = convertWordsToDigits(matches, false);
+  return firstDigit;
 }
 
-readLines(filePath).then((lines) => {
-  let result = 0;
-  lines.forEach((line) => {
-    const allDigits = getNumberFromString(line);
-    //console.log(allDigits);
-    const sums = allDigits[0] + allDigits[allDigits.length - 1];
-    //console.log(sums);
-    result += Number(sums);
+function getLastDigit(str) {
+  const regex = /eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[1-9]/;
+  const matches = str.match(regex);
+  const lastDigit = convertWordsToDigits(matches, true);
+  return lastDigit;
+}
+
+function convertWordsToDigits(wordArray, reverse) {
+  let convertedArray = [];
+  const firstWordToDigitMap = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+  const lastWordToDigits = {
+    eno: "1",
+    owt: "2",
+    eerht: "3",
+    ruof: "4",
+    evif: "5",
+    xis: "6",
+    neves: "7",
+    thgie: "8",
+    enin: "9",
+  };
+  if (!reverse) {
+    convertedArray = wordArray.map((word) => firstWordToDigitMap[word] || word);
+  } else {
+    convertedArray = wordArray.map((word) => lastWordToDigits[word] || word);
+  }
+  return convertedArray;
+}
+
+const final = () =>
+  readLines(filePath).then((lines) => {
+    let result = 0;
+    let allDigits = 0;
+    let firstDigit = 0;
+    let lastDigit = 0;
+    lines.forEach((line) => {
+      firstDigit = getFirstDigit(line);
+      lastDigit = getLastDigit(line.split("").reverse().join(""));
+
+      allDigits = firstDigit[0] + lastDigit[0];
+      result += Number(allDigits);
+    });
+
+    console.log(result);
   });
-  console.log(result);
-});
+
+final();
